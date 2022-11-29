96394acf97df511e1979951cee6ae4811a0c5900
kiana-jahanshid <64160703+kiana-jahanshid@users.noreply.github.com>
2022-11-29 18:42:00 +0330

    Delete Assignment_4 directory

--- Assignment_4/1_qrcode.py
+++
@@ -1,7 +0,0 @@
-import qrcode 
-
-name = input("\nEnter your name :")
-phone = input("Enter your phone number :")
-
-img = qrcode.make( "name : " + name +"\n"+ "phone number :" + phone)
-img.save("qrcode.png")
